0767d1414d504b5bad43f7110da9ce308e31fd39
rose <144841421+rosenode@users.noreply.github.com>
2025-03-17 13:40:32 -0400

    7 - Update toDo

--- js/app.js
+++ js/app.js
@@ -9,12 +9,22 @@ const input = document.getElementById("input");
 
 const list = document.getElementById("list");
 
-function addToDo(toDo) {
+const CHECK="fa-check-circle";
+const UNCHECK="fa-circle-thin";
+const LINE_THROUGH="lineThrough";
+
+function addToDo(toDo ,id,done ,trash) {
+
+    if(trash){return; }
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH: "";
+
     const text = <li class="item">
-        <i class="co fa fa-circle-thin" job="complete"></i>
-        <p class="text">${toDo}</p>
-        <i class="de fa fa-trash-o" job="delete"></i>
-    </li>
+        <i class="fa ${DONE} complete" job="complete" id="${id}"></i>
+        <p class="text ${LINE}">${toDo}</p>
+        <i class="de fa fa-trash-o" job="delete" id="${id}"></i>
+    </li>;
 
     const position = "beforeend"
     list.insertAdjacentHTML(position, text);
